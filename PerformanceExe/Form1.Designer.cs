9147c46fb3c8c0698399a5f7173cb45802c9fe88
Herdin Anhar <139094609+elherdin@users.noreply.github.com>
2024-01-09 10:02:30 +0700

    Add files via upload

--- PerformanceExe/Form1.Designer.cs
+++ PerformanceExe/Form1.Designer.cs
@@ -31,59 +31,36 @@ namespace PerformanceExe
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(87, 127);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button1.Location = new System.Drawing.Point(52, 142);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(189, 59);
+            this.button1.Size = new System.Drawing.Size(156, 45);
             this.button1.TabIndex = 0;
             this.button1.Text = "Upload File";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(428, 235);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(208, 63);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Count";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // label1
-            // 
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(82, 234);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(262, 64);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Execution Code Time";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
             // label2
             // 
             this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(660, 235);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(358, 702);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(449, 63);
+            this.label2.Size = new System.Drawing.Size(474, 40);
             this.label2.TabIndex = 3;
             this.label2.Text = "Time (ms): ";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -91,65 +68,113 @@ namespace PerformanceExe
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(321, 127);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox1.Location = new System.Drawing.Point(264, 142);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(506, 56);
+            this.textBox1.Size = new System.Drawing.Size(643, 45);
             this.textBox1.TabIndex = 4;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(87, 336);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox2.Location = new System.Drawing.Point(52, 228);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox2.Size = new System.Drawing.Size(1022, 321);
+            this.textBox2.Size = new System.Drawing.Size(1046, 417);
             this.textBox2.TabIndex = 5;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // label3
             // 
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(313, 27);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(417, 47);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(610, 66);
+            this.label3.Size = new System.Drawing.Size(305, 34);
             this.label3.TabIndex = 6;
-            this.label3.Text = "Performance - Execution Time";
+            this.label3.Text = "Execution Time";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(894, 125);
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button3.Location = new System.Drawing.Point(968, 142);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(215, 61);
+            this.button3.Size = new System.Drawing.Size(130, 45);
             this.button3.TabIndex = 7;
             this.button3.Text = "Upload Folder";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button5
+            // 
+            this.button5.AutoSize = true;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(54, 702);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(73, 40);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Help";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(145, 702);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 40);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Info";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtOutput.Location = new System.Drawing.Point(978, 702);
+            this.txtOutput.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(120, 40);
+            this.txtOutput.TabIndex = 11;
+            this.txtOutput.Text = "Export.CSV";
+            this.txtOutput.UseVisualStyleBackColor = true;
+            this.txtOutput.Click += new System.EventHandler(this.txtOutput_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 703);
+            this.ClientSize = new System.Drawing.Size(1167, 815);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Name = "Form1";
-            this.Text = "Execution Code";
+            this.Text = "form";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
@@ -170,13 +195,14 @@ namespace PerformanceExe
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button txtOutput;
     }
 }
 
--- PerformanceExe/Form1.cs
+++ PerformanceExe/Form1.cs
@@ -22,6 +22,8 @@ namespace PerformanceExe
             InitializeComponent();
         }
 
+        List<Tuple<string, string>> executionTimes = new List<Tuple<string, string>>();
+
         private void button1_Click(object sender, EventArgs e)
         {
             using (OpenFileDialog openFileDialog = new OpenFileDialog())
@@ -32,37 +34,28 @@ namespace PerformanceExe
                 if (openFileDialog.ShowDialog() == DialogResult.OK)
                 {
                     selectedFilePath = openFileDialog.FileName;
-                    textBox1.Text = selectedFilePath; 
+                    textBox1.Text = selectedFilePath;
+
+                    var stopwatch = new Stopwatch();
+                    stopwatch.Start();
+
+                    string fileContent = File.ReadAllText(selectedFilePath);
+                    textBox2.Text = fileContent;
+
+                    stopwatch.Stop();
+
+                    label2.Text = $"Time (ms) = {stopwatch.Elapsed.ToString()}";
+
+                    executionTimes.Add(new Tuple<string, string>(stopwatch.Elapsed.ToString(), selectedFilePath));
                 }
             }
-
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            if(!string.IsNullOrEmpty(selectedFilePath))
-            {
-                var stopwatch = new Stopwatch();
-
-                stopwatch.Start();
-
-                // Tempatkan kode pembacaan atau eksekusi file Anda di sisi sini.
-                // Misalnya: Membaca isi file dan menampilkan di MessageBox.
-                string fileContent = File.ReadAllText(selectedFilePath);
-                textBox2.Text = fileContent;
-
-                stopwatch.Stop();
-
-                MessageBox.Show("Execution Duration: \n" + stopwatch.Elapsed.ToString());
-                label2.Text = $"Time = {stopwatch.Elapsed.ToString()}";
-            }
-        }
 
         private void button3_Click(object sender, EventArgs e)
         {
             using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
             {
-               
                 if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
                 {
                     string selectedFolderPath = folderBrowserDialog.SelectedPath;
@@ -71,7 +64,6 @@ namespace PerformanceExe
                     var stopwatch = new Stopwatch();
                     stopwatch.Start();
 
-                    
                     string[] csFiles = Directory.GetFiles(selectedFolderPath, "*.cs");
                     StringBuilder allFileContent = new StringBuilder();
 
@@ -85,8 +77,9 @@ namespace PerformanceExe
 
                     stopwatch.Stop();
 
-                    MessageBox.Show("Execution Duration: \n" + stopwatch.Elapsed.ToString());
-                    label2.Text = $"Time = {stopwatch.Elapsed.ToString()}";
+                    label2.Text = $"Time (ms) = {stopwatch.Elapsed.ToString()}";
+
+                    executionTimes.Add(new Tuple<string, string>(stopwatch.Elapsed.ToString(), selectedFolderPath));
                 }
             }
         }
@@ -95,5 +88,118 @@ namespace PerformanceExe
         {
 
         }
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ShowHelp();
+        }
+
+        private void ShowHelp()
+        {
+            string help = OpenHelp();
+            MessageBox.Show(help, "User Guide", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private string OpenHelp()
+        {
+            StringBuilder help = new StringBuilder();
+
+            help.AppendLine("User Guide for Execution Time Application");
+            help.AppendLine();
+            help.AppendLine("1. Press the \"upload file\" option if you want to insert an individual file with the .cs format only.");
+            help.AppendLine("2. Choose the \"upload folder\" option to include a folder containing .cs files.");
+            help.AppendLine("3. The application will display the successfully executed time on the \"time (ms)\" bar.");
+            help.AppendLine("4. The recorded time is presented in milliseconds (ms).");
+            help.AppendLine("5. Press the export.csv button to download the executed time");
+           
+
+            return help.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ShowInfo();
+        }
+
+        private void ShowInfo()
+        {
+            string info = OpenInfo();
+            MessageBox.Show(info, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private string OpenInfo()
+        {
+            StringBuilder info = new StringBuilder();
+
+            info.AppendLine("Execution Time: ");
+            info.AppendLine();
+            info.AppendLine("\"The execution time  is the total amount of time that the process executes; that time is generally independent of the initiation time but often depends on the input data.\"[1]");
+            info.AppendLine("[1] https://www.sciencedirect.com/topics/computer-science/total-execution-time");
+            info.AppendLine();
+            info.AppendLine("Matrics: ");
+            info.AppendLine();
+            info.AppendLine("1. CPU Usage:");
+            info.AppendLine("    a. Description:");
+            info.AppendLine("        The CPU usage metric represents the percentage of the computer's processing power utilized during the execution time testing.");
+            info.AppendLine("    b. Formula:");
+            info.AppendLine("        CPU Usage = Current CPU Usage");
+            info.AppendLine("2. RAM Usage:");
+            info.AppendLine("    a. Description:");
+            info.AppendLine("        The RAM usage metric indicates the amount of computer memory used during the execution time testing.");
+            info.AppendLine("    b. Formula:");
+            info.AppendLine("        RAM Usage = Current RAM Usage");
+            info.AppendLine("3. Successful Request:");
+            info.AppendLine("    a. Description:");
+            info.AppendLine("        Successful Request is the time it takes for the application to read and display the contents of the selected file or folder.");
+            info.AppendLine("    b. Formula:");
+            info.AppendLine("        Execution Time = Time Required to Read and Display the Contents of a File or Folder");
+
+            return info.ToString();
+        }
+
+        private void txtOutput_Click(object sender, EventArgs e)
+        {
+            if (executionTimes.Count > 0)
+            {
+                try
+                {
+                    SaveFileDialog saveFileDialog = new SaveFileDialog();
+                    saveFileDialog.Filter = "CSV Files (*.csv)|*.csv";
+                    saveFileDialog.Title = "Save Metrics to CSV";
+                    saveFileDialog.DefaultExt = "csv";
+
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        string csvFilePath = Path.Combine(saveFileDialog.InitialDirectory, saveFileDialog.FileName);
+
+                        // Write the header to the CSV file
+                        StringBuilder csvContent = new StringBuilder("Execution Time (ms),Processed File\n");
+
+                        // Write each execution time and processed file name to the CSV file
+                        foreach (var tuple in executionTimes)
+                        {
+                            csvContent.AppendLine($"{tuple.Item1},{tuple.Item2}");
+                        }
+
+                        // Write the content of txtOutput to the selected CSV file
+                        File.WriteAllText(csvFilePath, csvContent.ToString());
+
+                        MessageBox.Show("Metrics saved to CSV file.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("No metrics to export", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+
     }
 }
